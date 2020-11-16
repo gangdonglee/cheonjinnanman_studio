512d09bdebe9c02b3a25c2f7aecba020434a0db6
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2020-11-16 23:15:40 +0900

    1116

--- custom.js
+++ custom.js
@@ -16,19 +16,13 @@ $(function(){
         $.scrollTo(this.hash || 0, 900)
     })
 
-
-    /* Change CSS with Scroll */
-    $(window).scroll(function () {
-      if($(window).scrollTop() > 50)
-      {
-        $('header, .gototop').addClass('active')
-      }
-      else
-      {
-        $('header, .gototop').removeClass('active')
-      }
-      
-  
+    //slick.js
+    $('.history-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
     })
 
 
