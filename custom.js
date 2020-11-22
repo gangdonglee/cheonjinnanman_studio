76495f625f9853e5343e7e072b651f762209c3a2
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2020-11-22 17:07:13 +0900

    1122

--- custom.js
+++ custom.js
@@ -26,5 +26,15 @@ $(function(){
       
     })
 
+    //slick.js : project photo
+    $('.project-photo').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear'  
+    })
 
 })
